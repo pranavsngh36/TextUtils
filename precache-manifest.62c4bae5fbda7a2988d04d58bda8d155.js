c5ab84de94f4d408d69eb400c8cab7946a4de79d
pranavsngh36 <pranavsngh36@gmail.com>
2022-07-17 00:03:22 +0530

    Updates

---
+++ precache-manifest.62c4bae5fbda7a2988d04d58bda8d155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c3e92e1102f41458db8",
+    "url": "/TextUtils/static/css/main.f7cc7c2d.chunk.css"
+  },
+  {
+    "revision": "3c3e92e1102f41458db8",
+    "url": "/TextUtils/static/js/main.3c3e92e1.chunk.js"
+  },
+  {
+    "revision": "833a7611ce42bb2c9054",
+    "url": "/TextUtils/static/js/1.833a7611.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "f3a606898f52ee4a200e",
+    "url": "/TextUtils/static/js/runtime~main.f3a60689.js"
+  },
+  {
+    "revision": "811262581e0d7976a580815abb403510",
+    "url": "/TextUtils/index.html"
+  }
+];
